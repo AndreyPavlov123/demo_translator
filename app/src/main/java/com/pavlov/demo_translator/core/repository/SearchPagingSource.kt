499c58e86cdf790b94cdd6dc11b69763698ffe94
Andrey Pavlov <andreypavlov007@gmail.com>
2020-09-07 19:51:33 +0300

    improve paging behaviour

--- app/src/main/java/com/pavlov/demo_translator/core/repository/SearchPagingSource.kt
+++ app/src/main/java/com/pavlov/demo_translator/core/repository/SearchPagingSource.kt
@@ -8,10 +8,12 @@ import com.pavlov.demo_translator.core.api.MeaningShortRoot
 class SearchPagingSource constructor(private val api: Api, private val query: String)
     : PagingSource<Int, MeaningShortRoot>() {
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, MeaningShortRoot> = try {
-        val nextPageNumber = params.key ?: 1
-        val searchResult = api.search(query, page = nextPageNumber, pageSize = params.loadSize)
+        val page = params.key ?: 1
+        val searchResult = api.search(query, page = page, pageSize = params.loadSize)
         val data = searchResult.body()!!
-        LoadResult.Page(data, null, if (data.size < params.loadSize) null else nextPageNumber + 1)
+        LoadResult.Page(data,
+            if (page == 1) null else page - 1,
+            if (data.size < params.loadSize) null else page + 1)
     } catch (e: Exception) {
         LoadResult.Error(e)
     }
--- app/src/main/java/com/pavlov/demo_translator/ui/search/SearchFragment.kt
+++ app/src/main/java/com/pavlov/demo_translator/ui/search/SearchFragment.kt
@@ -3,18 +3,24 @@ package com.pavlov.demo_translator.ui.search
 import android.os.Bundle
 import android.view.*
 import androidx.appcompat.widget.SearchView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.paging.ExperimentalPagingApi
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.pavlov.demo_translator.R
 import com.pavlov.demo_translator.databinding.FragmentSearchBinding
 import com.pavlov.demo_translator.ui.meaning.MeaningFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_search.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.distinctUntilChangedBy
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -27,8 +33,6 @@ class SearchFragment : Fragment() {
         fun newInstance() = SearchFragment()
     }
 
-    private lateinit var searchItem: MenuItem
-    private lateinit var searchView: SearchView
     private val viewModel: SearchViewModel by viewModels()
     private lateinit var pagingAdapter: SearchAdapter
 
@@ -44,7 +48,10 @@ class SearchFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         val binding = FragmentSearchBinding.inflate(inflater, container, false)
-        binding.searchRecyclerView.adapter = pagingAdapter
+        binding.searchRecyclerView.adapter = pagingAdapter.withLoadStateHeaderAndFooter(
+            header = NetworkLoadStateAdapter(pagingAdapter),
+            footer = NetworkLoadStateAdapter(pagingAdapter)
+        )
         binding.searchRecyclerView.layoutManager = LinearLayoutManager(context)
         return binding.root
     }
@@ -53,16 +60,31 @@ class SearchFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
         lifecycleScope.launch {
-            viewModel.searchPagingFlow.collectLatest { pagingData ->
-                pagingAdapter.submitData(pagingData)
+            viewModel.searchPagingFlow.collectLatest {
+                pagingAdapter.submitData(it)
             }
+        }
+
+        lifecycleScope.launch {
+            pagingAdapter.loadStateFlow.collectLatest { loadStates ->
+                progressBar.isVisible = loadStates.refresh is LoadState.Loading
+            }
+        }
+
+        lifecycleScope.launch {
+            pagingAdapter.loadStateFlow
+                // Only emit when REFRESH LoadState for RemoteMediator changes.
+                .distinctUntilChangedBy { it.refresh }
+                // Only react to cases where Remote REFRESH completes i.e., NotLoading.
+                .filter { it.refresh is LoadState.NotLoading }
+                .collect { searchRecyclerView.scrollToPosition(0) }
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.menu_search, menu)
-        searchItem = menu.findItem(R.id.action_search)
-        searchView = searchItem.actionView as SearchView
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
 
         searchView.setOnCloseListener {
             viewModel.search("")
@@ -81,7 +103,6 @@ class SearchFragment : Fragment() {
             }
         })
 
-        searchView.isIconified = false
         searchItem.expandActionView()
 
         return super.onCreateOptionsMenu(menu, inflater)
