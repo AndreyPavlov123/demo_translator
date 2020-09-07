26b9de24ee5834e93365cc641f9a4d1fb1df605a
Andrey Pavlov <andreypavlov007@gmail.com>
2020-09-07 14:30:56 +0300

    rework api data

--- app/src/main/java/com/pavlov/demo_translator/core/api/data/Data.kt
+++ app/src/main/java/com/pavlov/demo_translator/core/api/data/Data.kt
@@ -1,63 +1,69 @@
 package com.pavlov.demo_translator.core.api.data
 
-class Translation {
-    var text: String? = null
-    var note: String? = null
-}
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
-class MeaningShort {
-    var id: Int? = null
-    var partOfSpeechCode: String? = null
-    var translation: Translation? = null
-    var previewUrl: String? = null
-    var imageUrl: String? = null
-    var transcription: String? = null
+@Parcelize
+data class MeaningShort (
+    var id: Int? = null,
+    var partOfSpeechCode: String? = null,
+    var translation: Translation? = null,
+    var previewUrl: String? = null,
+    var imageUrl: String? = null,
+    var transcription: String? = null,
     var soundUrl: String? = null
-}
+) : Parcelable
 
-class MeaningShortRoot {
-    var id: Int? = null
-    var text: String? = null
+@Parcelize
+data class MeaningShortRoot (
+    var id: Int? = null,
+    var text: String? = null,
     var meanings: List<MeaningShort>? = null
-}
+) : Parcelable
 
-class Image {
+@Parcelize
+data class Translation (
+    var text: String? = null,
+    var note: String? = null
+) : Parcelable
+
+data class Image (
     var url: String? = null
-}
+)
 
-class Definition {
-    var text: String? = null
+data class Definition (
+    var text: String? = null,
     var soundUrl: String? = null
-}
+)
 
-class MeaningsWithSimilarTranslation {
-    var meaningId = 0
-    var frequencyPercent: String? = null
-    var partOfSpeechAbbreviation: String? = null
+data class MeaningsWithSimilarTranslation (
+    var meaningId: Int? = null,
+    var frequencyPercent: String? = null,
+    var partOfSpeechAbbreviation: String? = null,
     var translation: Translation? = null
-}
+)
 
-class AlternativeTranslation {
-    var text: String? = null
+data class AlternativeTranslation (
+    var text: String? = null,
     var translation: Translation? = null
-}
+)
 
-class MeaningFull {
-    var id: String? = null
-    var wordId = 0
-    var difficultyLevel = 0
-    var partOfSpeechCode: String? = null
-    var prefix: String? = null
-    var text: String? = null
-    var soundUrl: String? = null
-    var transcription: String? = null
-    var properties: Map<String, Any?>? = null
-    var updatedAt: String? = null
-    var mnemonics: String? = null
-    var translation: Translation? = null
-    var images: List<Image>? = null
-    var definition: Definition? = null
-    var examples: List<Definition>? = null
-    var meaningsWithSimilarTranslation: List<MeaningsWithSimilarTranslation>? = null
+data class MeaningFull (
+    var id: String? = null,
+    var wordId: Int? = null,
+    var difficultyLevel: Int? = null,
+    var partOfSpeechCode: String? = null,
+    var prefix: String? = null,
+    var text: String? = null,
+    var soundUrl: String? = null,
+    var transcription: String? = null,
+    var properties: Map<String, Any?>? = null,
+    var updatedAt: String? = null,
+    var mnemonics: String? = null,
+    var translation: Translation? = null,
+    var images: List<Image>? = null,
+    var definition: Definition? = null,
+    var examples: List<Definition>? = null,
+    var meaningsWithSimilarTranslation: List<MeaningsWithSimilarTranslation>? = null,
     var alternativeTranslations: List<AlternativeTranslation>? = null
-}
+)
