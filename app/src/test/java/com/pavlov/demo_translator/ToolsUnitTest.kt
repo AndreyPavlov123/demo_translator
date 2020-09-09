481489f59c6ee267ee43df3c5549c03253d39e52
Andrey Pavlov <andreypavlov007@gmail.com>
2020-09-09 03:19:42 +0300

    add unit test

---
+++ app/src/test/java/com/pavlov/demo_translator/ToolsUnitTest.kt
@@ -0,0 +1,17 @@
+package com.pavlov.demo_translator
+
+import com.pavlov.demo_translator.core.api.data.correctUrl
+import org.junit.Test
+
+import org.junit.Assert.*
+import java.net.URI
+
+class ToolsUnitTest {
+
+    @Test
+    fun urlCorrection_IsCorrect() {
+        assertEquals(4, 2 + 2)
+        val uri = URI.create("//d.android.com/tools/testing".correctUrl())
+        assertEquals(uri.scheme, "https")
+    }
+}
